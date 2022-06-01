fe59c243bddfa819e1692d7a8e61f8714fe68538
Leif Liddy <leifliddy@users.noreply.github.com>
2022-06-01 08:32:40 +0200

    set fs labels

--- build.sh
+++ build.sh
@@ -87,9 +87,9 @@ prepare_usb_device() {
     #echo -e 'o\ny\nn\n\n\n+600M\nef00\nn\n\n\n+1G\n8300\nn\n\n\n+5G\n8300\nw\ny\n' | gdisk "$usb_device"
     # root parition will take up all remaining space
     echo -e 'o\ny\nn\n\n\n+600M\nef00\nn\n\n\n+1G\n8300\nn\n\n\n\n8300\nw\ny\n' | gdisk "$usb_device"
-    mkfs.vfat -F 32 -i $(echo $EFI_UUID | tr -d '-') "$usb_device"1 || mkfs.vfat -F 32 -i $(echo $EFI_UUID | tr -d '-') "$usb_device"p1
-    mkfs.ext4 -U $BOOT_UUID -F "$usb_device"2 || mkfs.ext4 -U $BOOT_UUID -F "$usb_device"p2
-    mkfs.ext4 -U $ROOT_UUID -F "$usb_device"3 || mkfs.ext4 -U $ROOT_UUID -F "$usb_device"p3
+    mkfs.vfat -F 32 -n 'EFI-USB-FED' -i $(echo $EFI_UUID | tr -d '-') "$usb_device"1 || mkfs.vfat -F 32 -n 'EFI-USB-FED' -i $(echo $EFI_UUID | tr -d '-') "$usb_device"p1
+    mkfs.ext4 -O '^metadata_csum' -U $BOOT_UUID -L 'fedora-usb-boot' -F "$usb_device"2 || mkfs.ext4 -O '^metadata_csum' -U $BOOT_UUID -L 'fedora-usb-boot' -F "$usb_device"p2
+    mkfs.ext4 -O '^metadata_csum' -U $ROOT_UUID -L 'fedora-usb-root' -F "$usb_device"3 || mkfs.ext4 -O '^metadata_csum' -U $ROOT_UUID -L 'fedora-usb-root' -F "$usb_device"p3
     systemctl daemon-reload
 
     if [ $(blkid | egrep -i "$EFI_UUID|$BOOT_UUID|$ROOT_UUID" | wc -l) -ne 3 ]; then
