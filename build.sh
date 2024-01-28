30d5e589cd0bad6054e588d52d4e62988e385850
Leif Liddy <leif.liddy@gmail.com>
2024-01-28 10:19:12 +0100

    minor improvements

--- build.sh
+++ build.sh
@@ -183,9 +183,10 @@ install_usb() {
     rm -f $mkosi_rootfs/var/cache/dnf/*
     echo '### Mounting usb partitions and copying files'
     mount -U $ROOT_UUID $mnt_usb
-    rsync -aHAX --delete --exclude '/tmp/*' --exclude '/boot/*' --exclude '/efi' $mkosi_rootfs/ $mnt_usb
+    rsync -aHAX --exclude '/tmp/*' --exclude '/boot/*' --exclude '/efi' $mkosi_rootfs/ $mnt_usb
     mount -U $BOOT_UUID $mnt_usb/boot
-    rsync -aHAX --delete $mkosi_rootfs/boot/ $mnt_usb/boot
+    echo "rsync -aHAX --exclude '/efi/*' $mkosi_rootfs/boot/ $mnt_usb/boot"
+    rsync -aHAX --exclude '/efi/*' $mkosi_rootfs/boot/ $mnt_usb/boot
     mount -U $EFI_UUID $mnt_usb/boot/efi
     echo "rsync -aH $mkosi_rootfs/boot/efi/ $mnt_usb/boot/efi"
     rsync -aH $mkosi_rootfs/boot/efi/ $mnt_usb/boot/efi
